8a7a6dbd9a95e74c334897e334e0c17c51c922e2
Benson Muriithi <mwngmuriithi@gmail.com>
2016-10-16 01:21:44 +0300

    Make executables/__init__.py flexible to accomodate path changes

--- System Util/sysutil/executables/__init__.py
+++ System Util/sysutil/executables/__init__.py
@@ -1,6 +1,5 @@
 import os
 
 #make these values constant and accessible by sysutil
-eject = os.path.abspath("executables\\eject.exe")
-recycle = os.path.abspath("executables\\recycle.exe")
-
+eject = os.path.join(os.path.dirname(os.path.abspath(__file__)), "eject.exe")
+recycle = os.path.join(os.path.dirname(os.path.abspath(__file__)), "recycle.exe")
